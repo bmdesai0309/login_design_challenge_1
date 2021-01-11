6647ab07fca7e05f7b8fae58ff6c49e589721f69
bmdesai0309 <bmdesai1685@gmail.com>
2021-01-11 16:33:10 +0530

    design complete

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -44,19 +44,46 @@ class _SignUpState extends State<SignUp> {
     );
   }
 
-  Padding buildText(ThemeData theme, String text) {
+  Padding buildText(ThemeData theme, String text, double fontSize) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Text(
         text,
         style: TextStyle(
           color: theme.primaryColorDark,
-          fontSize: 12.0,
+          fontSize: fontSize,
         ),
       ),
     );
   }
 
+  Padding buildInputField(
+      String labelText, String hintText, bool isobsured, ThemeData theme) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: labelText,
+          hintText: hintText,
+          hintStyle: TextStyle(color: theme.primaryColorDark),
+        ),
+        obscureText: isobsured,
+      ),
+    );
+  }
+
+  SizedBox buildSignupBtn(ThemeData theme) {
+    return SizedBox(
+      width: 250.0,
+      child: RaisedButton(
+        onPressed: () {},
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+        padding: EdgeInsets.symmetric(vertical: 12.0),
+        color: theme.buttonColor,
+        child: Text('Sign up with email'),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,9 +102,18 @@ class _SignUpState extends State<SignUp> {
                 children: [
                   buildTitle(theme),
                   buildFacebookBtn(),
-                  buildText(theme,'or'),
-                  buildText(theme,'Sign up with your email address'),
-
+                  buildText(theme, 'or', 12.0),
+                  buildText(theme, 'Sign up with your email address', 12.0),
+                  buildInputField('Email', 'your@email.com', false, theme),
+                  buildInputField('Password', '', true, theme),
+                  SizedBox(
+                    height: 18.0,
+                  ),
+                  buildSignupBtn(theme),
+                  buildText(theme, 'By signing up you agree with our tearms & conditions', 8.0),
+                  SizedBox(
+                    height: 18.0,
+                  ),
                 ],
               ),
             ),
@@ -86,5 +122,4 @@ class _SignUpState extends State<SignUp> {
       ],
     ));
   }
-
 }
